4561ab3e62fc9373396a16958018973a7c3a2249
Kushal Niroula <niroulakushal31@gmail.com>
2022-08-12 13:14:42 +0545

    feat: example usage in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import VNepaliDatePicker from './components/VNepaliDatePicker.vue';
+const date = ref<Date | null>(new Date());
+const max = ref<Date | null>(new Date());
 </script>
 
 <template>
-  <VNepaliDatePicker/>
+  <VNepaliDatePicker v-model="date" :allow-clear="true" :year-select="true" :month-select="true" :max="max">
+  <template #clear-btn="{onClear}">
+    <span class="clear-btn" @click.prevent="onClear">
+        ❌
+    </span>
+  </template>
+  </VNepaliDatePicker>
+  <h3>
+    Selected Date: {{date?.toDateString()}}
+  </h3>
 </template>
+
+<style scoped>
+.clear-btn {
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+</style>
