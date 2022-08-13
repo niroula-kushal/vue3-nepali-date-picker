02ff9081179d6803a32df6992f507a1de8c2110d
Kushal Niroula <niroulakushal31@gmail.com>
2022-08-13 10:32:05 +0545

    feat: add docs build

---
+++ vite-docs.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [vue()]
+})
