998a4f5566df83a8946e0e31d04ec6cc1361ed0b
Kushal Niroula <niroulakushal31@gmail.com>
2022-09-10 16:19:28 +0545

    fix: NepaliFunctions export issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
-export {default as VNepaliDatePicker} from './VNepaliDatePicker.vue';
-export {default as NepaliFunctions } from '../nepali-date-picker/NepaliFunctions.js';
+import NepaliFunctions from '../nepali-date-picker/NepaliFunctions.js';
+import VNepaliDatePicker from './VNepaliDatePicker.vue';
+
+export { VNepaliDatePicker, NepaliFunctions };
